e2cee9d1f56a681a77702ecbdc8fee0fa9b3c905
Christopher Schuster <cs@livoris.net>
2016-12-15 16:37:29 -0800

    fixed flatMap bug

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export function flatMap<A,B>(a: Array<A>, f: (a: A) => Array<B>): Array<B> {
-  return a.map(f).reduce((a,b) => a.concat(b));
+  return a.map(f).reduce((a,b) => a.concat(b), []);
 }
 
 export function pushAll<A>(a: Array<A>, b: Array<A>) {
