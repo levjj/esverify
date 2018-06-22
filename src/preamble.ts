3648cedb083d62d606050e30094f2410c24f9da5
Christopher Schuster <cs@livoris.net>
2018-06-21 21:42:41 -0700

    fix(preamble): store preamble as opaque string to survive minification

--- src/preamble.ts
+++ src/preamble.ts
@@ -109,13 +109,103 @@ class PreambleGenrator extends VCGenerator {
   }
 }
 
+const VCGEN_PREAMBLE = `
+const Number = {
+  isInteger: function (n) {
+    ensures(pure());
+    return [ '_builtin_', '(jsbool (is-jsint ', n, '))'];
+  }
+};
+
+class String {
+  constructor (_str_) {
+    this._str_ = _str_;
+  }
+  invariant () {
+    return typeof this === 'string' || typeof this._str_ === 'string';
+  }
+  substr (from, len) {
+    requires(Number.isInteger(from));
+    requires(Number.isInteger(len));
+    requires(from >= 0);
+    requires(len >= 0);
+    ensures(pure());
+    return [
+      '_builtin_',
+      '(jsstr (str.substr (strv (ite (is-jsstr ', this, ') ', this, ' (String-_str_ ', this, '))) ',
+      '(intv ', from, ') (intv ', len, ')))'];
+  }
+  substring (from, to) {
+    requires(Number.isInteger(from));
+    requires(Number.isInteger(to));
+    requires(from >= 0);
+    requires(from < this.length);
+    requires(to >= from);
+    requires(to < this.length);
+    ensures(pure());
+    return [
+      '_builtin_',
+      '(jsstr (str.substr (strv (ite (is-jsstr ', this, ') ', this, ' (String-_str_ ', this, '))) ',
+      '(intv ', from, ') (intv ', to - from, ')))'];
+  }
+}
+
+class Array {
+  invariant () {
+    return this.length >= 0;
+  }
+  slice (from, to) {
+    requires(Number.isInteger(from));
+    requires(Number.isInteger(to));
+    requires(from >= 0);
+    requires(from < this.length);
+    requires(to >= from);
+    requires(to < this.length);
+    ensures(y => y.every((ele, idx) => ele === this[idx + from]));
+    ensures(y => y.length === to - from);
+    ensures(pure());
+  }
+}
+
+const Math = {
+  max: function (n, m) {
+    requires(typeof n === 'number');
+    requires(typeof m === 'number');
+    ensures(pure());
+    ensures(z => z === (n >= m ? n : m));
+  },
+  random: function () {
+    ensures(pure());
+    ensures(z => typeof z === 'number' && 0 <= z && z < 1.0);
+  },
+  trunc: function (n) {
+    requires(typeof n === 'number');
+    return [ '_builtin_', '(jsint (_toint ', n, '))'];
+  }
+};
+
+function parseInt (s, n) {
+  requires(typeof s === 'string');
+  requires(n === 10);
+  ensures(pure());
+  return [ '_builtin_', '(jsint (str.to.int (strv ', s, ')))'];
+}
+
+const console = {
+  log: function (arg) {
+    ensures(y => pure() && y === undefined);
+  }
+};
+
+function alert (arg) {
+  ensures(y => pure() && y === undefined);
+}`;
+
 let cachedPreamble: Preamble | null = null;
 
 export function generatePreamble (): Preamble {
   if (cachedPreamble === null) {
-    let preambleSource = preamble.toString();
-    preambleSource = preambleSource.substring(21, preambleSource.length - 1); // extract body from function
-    let preambleProgram: Syntax.Program = sourceAsJavaScript(preambleSource);
+    let preambleProgram: Syntax.Program = sourceAsJavaScript(VCGEN_PREAMBLE);
     resolveNames(preambleProgram, false);
     const vcgen = new PreambleGenrator(new Set(), 0, 0, new Set(), new Set(), [], [], tru);
     vcgen.visitProgram(preambleProgram);
@@ -129,142 +219,4 @@ export function generatePreamble (): Preamble {
     vars: new Set([...cachedPreamble.vars]),
     prop: copy(cachedPreamble.prop)
   };
-}
-
-// --- javascript preamble ---
-
-declare const requires: (x: boolean) => void;
-declare const ensures: (x: boolean | ((y: any) => boolean)) => void;
-declare const pure: () => boolean;
-
-function preamble () {
-  /* tslint:disable:no-unused-expression */
-  /* tslint:disable:strict-type-predicates */
-  /* tslint:disable:variable-name */
-
-  // @ts-ignore: var never used
-  const Number = {
-
-    isInteger: function (n: number): boolean {
-      ensures(pure());
-    // @ts-ignore: var never used
-      return [ '_builtin_', '(jsbool (is-jsint ', n, '))'];
-    }
-  };
-
-  // @ts-ignore: class never used
-  class String {
-
-    _str_: string;
-    // @ts-ignore: not assigned in constructors
-    length: number;
-
-    constructor (_str_: string) {
-      this._str_ = _str_;
-    }
-
-    invariant () {
-      return typeof this === 'string' || typeof this._str_ === 'string';
-    }
-
-    substr (from: number, len: number) {
-      requires(Number.isInteger(from));
-      requires(Number.isInteger(len));
-      requires(from >= 0);
-      requires(len >= 0);
-
-      ensures(pure());
-
-      return [
-        '_builtin_',
-        '(jsstr (str.substr (strv (ite (is-jsstr ', this, ') ', this, ' (String-_str_ ', this, '))) ',
-        '(intv ', from, ') (intv ', len, ')))'];
-    }
-
-    substring (from: number, to: number) {
-      requires(Number.isInteger(from));
-      requires(Number.isInteger(to));
-      requires(from >= 0);
-      requires(from < this.length);
-      requires(to >= from);
-      requires(to < this.length);
-
-      ensures(pure());
-
-      return [
-        '_builtin_',
-        '(jsstr (str.substr (strv (ite (is-jsstr ', this, ') ', this, ' (String-_str_ ', this, '))) ',
-        '(intv ', from, ') (intv ', to - from, ')))'];
-    }
-  }
-
-  // @ts-ignore: class never used
-  class Array {
-
-    // @ts-ignore: not assigned in constructors
-    length: number;
-
-    invariant () {
-      return this.length >= 0;
-    }
-
-    slice (from: number, to: number) {
-      requires(Number.isInteger(from));
-      requires(Number.isInteger(to));
-      requires(from >= 0);
-      requires(from < this.length);
-      requires(to >= from);
-      requires(to < this.length);
-
-      // @ts-ignore: indexing this
-      ensures(y => y.every((ele, idx) => ele === this[idx + from]));
-      ensures(y => y.length === to - from);
-      ensures(pure());
-    }
-  }
-
-  // @ts-ignore: var never used
-  const Math = {
-
-    max: function (n: number, m: number) {
-      requires(typeof n === 'number');
-      requires(typeof m === 'number');
-
-      ensures(pure());
-      ensures(z => z === (n >= m ? n : m));
-    },
-
-    random: function () {
-      ensures(pure());
-      ensures(z => typeof z === 'number' && 0 <= z && z < 1.0);
-    },
-
-    trunc: function (n: number) {
-      requires(typeof n === 'number');
-      return [ '_builtin_', '(jsint (_toint ', n, '))'];
-    }
-  };
-
-  // @ts-ignore: function never used
-  function parseInt (s: string, n) {
-    requires(typeof s === 'string');
-    requires(n === 10);
-
-    ensures(pure());
-
-    return [ '_builtin_', '(jsint (str.to.int (strv ', s, ')))'];
-  }
-
-  // @ts-ignore: variable only initialized, never read
-  const console = {
-
-    log: function (arg: any) {
-      ensures(y => pure() && y === undefined);
-    }
-  };
-
-  // @ts-ignore: function never used
-  function alert (arg: any) {
-    ensures(y => pure() && y === undefined);
-  }
 }
