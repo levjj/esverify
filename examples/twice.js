2e0d16104a77d94e443fd022406b409c6407549a
Christopher Schuster <cs@livoris.net>
2018-06-23 16:35:47 -0700

    docs(examples): updated twice example to return a curried function

--- examples/twice.js
+++ examples/twice.js
@@ -2,18 +2,21 @@
 function inc (n) {
   requires(Number.isInteger(n));
   ensures(res => Number.isInteger(res) && res > n);
-
   return n + 1;
 }
 
-function twice (f, n) {
-  requires(spec(f, x => Number.isInteger(x), (x, y) => Number.isInteger(y) && y > x));
-  requires(Number.isInteger(n));
-  ensures(res => res > n + 1);
-
-  return f(f(n));
+function twice (f) {
+  requires(spec(f, (x) => Number.isInteger(x),
+                   (x, y) => Number.isInteger(y) && y > x));
+  ensures(g => spec(g, (x) => Number.isInteger(x),
+                       (x, y) => Number.isInteger(y) && y > x + 1));
+  return function (n) {
+    requires(Number.isInteger(n));
+    ensures(res => Number.isInteger(res) && res > n + 1);
+    return f(f(n));
+  };
 }
 
-const x = 3;
-const y = twice(inc, x);
+const incTwice = twice(inc);
+const y = incTwice(3);
 assert(y > 4);
